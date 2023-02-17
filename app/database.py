c4045c82feba012ae5eb4c2b1bc40976dbb2baf1
abrshtg <abrshtgam@gmail.com>
2023-02-17 17:14:40 +0300

    add procfile

--- app/database.py
+++ app/database.py
@@ -1,16 +1,8 @@
 from pymongo import MongoClient
+import os
+USERNAME = os.environ.get('BUCHI')
+PASSWORD = os.environ.get('BUCHI_PASS')
 
-# replace <username>, <password>, <dbname>, and <clustername> with your actual values
-# connection_string = f"mongodb+srv://<username>:<password>@<clustername>.mongodb.net/<dbname>?retryWrites=true&w=majority"
-# client = MongoClient(connection_string)
-
-# # test the connection
-# db = client.test
-# collection = db.test_collection
-# collection.insert_one({'test': 'test'})
-# This will create a new document in a collection called test_collection in the test database of your MongoDB cluster.
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(
+    f"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.mg1yqdj.mongodb.net/?retryWrites=true&w=majority")
 db = client["buchidb"]
-# adoptions = db["adoptions"]
-# customers = db["customers"]
-# pets = db["pets"]
